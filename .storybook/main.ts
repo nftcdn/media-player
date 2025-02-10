376e51cc78574d7a8d5e66b796f1f2116ea2e4fb
Smaug <smaug@nftcdn.io>
2025-02-10 21:57:12 +0100

    storybook: convert main config to typescript

---
+++ .storybook/main.ts
@@ -0,0 +1,18 @@
+import type { StorybookConfig } from '@web/storybook-framework-web-components';
+
+const config: StorybookConfig = {
+  // Required
+  framework: '@web/storybook-framework-web-components',
+  stories: ['../**/dist/stories/*.stories.{js,md,mdx}'],
+
+  // Optional
+  core: {
+    disableTelemetry: true,
+  },
+  addons: ['@storybook/addon-backgrounds', '@storybook/addon-controls'],
+  docs: {
+    autodocs: 'tag',
+  },
+};
+
+export default config;
